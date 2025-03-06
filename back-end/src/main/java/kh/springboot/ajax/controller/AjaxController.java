818ea6d71352105028cf2897b1317b54cce8f20b
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-03-06 14:44:53 +0900

    공공데이터

--- back-end/src/main/java/kh/springboot/ajax/controller/AjaxController.java
+++ back-end/src/main/java/kh/springboot/ajax/controller/AjaxController.java
@@ -1,7 +1,13 @@
 package kh.springboot.ajax.controller;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -203,4 +209,71 @@ public class AjaxController {
 		return result;
 	}
 
+	@GetMapping("/weather")
+	public String getWeather() {
+		StringBuilder sb = new StringBuilder();
+		try {
+			StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"); /*URL*/
+	        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=zHANegkooKdHDCkm9tsH9DdjJoFFHnMByKMlFNG5AiI6ABG%2Bmlx3Pw7NUYw%2B2VeZ17HD5FMOCVMIyqTgrkaz1A%3D%3D"); /*Service Key*/
+	        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+	        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8")); /*한 페이지 결과 수*/
+	        urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default: XML*/
+	        
+	        //날짜 
+	        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd HHmm");
+	        String now = sdf.format(new Date());
+	        System.out.println("now : "+now);
+	        String[] dayTime = now.split(" ");
+	        System.out.println("dayTime[0] : "+dayTime[0]);
+	        System.out.println("dayTime[1] : "+dayTime[1]);
+	        urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode(dayTime[0], "UTF-8")); /*‘21년 6월 28일 발표*/
+	        
+	        //시간
+	        int[] baseTime = {200, 500, 800, 1100, 1400, 1700, 2000, 2300};
+	        int index = 99;
+	        for(int i=0; i<baseTime.length;i++) {
+	        	if(Integer.parseInt(dayTime[1])<= baseTime[i]) {
+	        		index = i -1;
+	        		if (i==0) {
+	        			index = i;
+	        		}
+	        		break;
+	        	}
+	        }
+	        if(index==99) {
+	        	dayTime[1]="2300";
+	        }
+	        //0800 02300
+	        dayTime[1] = ("0" + baseTime[index]).substring(("0" + baseTime[index]).length()-4);
+	        System.out.println("dayTime[1] : "+dayTime[1]);
+	        
+	        urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode(dayTime[1], "UTF-8")); /*06시 발표(정시단위) */
+	        urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode("58", "UTF-8")); /*예보지점의 X 좌표값*/
+	        urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode("127", "UTF-8")); /*예보지점의 Y 좌표값*/
+	        
+	        URL url = (new URI(urlBuilder.toString())).toURL();
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setRequestMethod("GET");
+	        conn.setRequestProperty("Content-type", "application/json");
+	        //System.out.println("Response code: " + conn.getResponseCode());
+	        
+	        BufferedReader rd;
+	        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+	            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	        } else {
+	            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+	        }
+	        
+	        String line;
+	        while ((line = rd.readLine()) != null) {
+	            sb.append(line);
+	        }
+	        rd.close();
+	        conn.disconnect();
+	        System.out.println(sb.toString());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return sb.toString();
+	}
 }
