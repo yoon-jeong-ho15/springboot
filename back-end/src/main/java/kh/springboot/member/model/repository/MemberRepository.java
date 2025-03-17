abe50c27c60c20fe8aa4922cc6250bf95b308654
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-03-17 14:06:08 +0900

    jpa 설정중

--- back-end/src/main/java/kh/springboot/member/model/repository/MemberRepository.java
+++ back-end/src/main/java/kh/springboot/member/model/repository/MemberRepository.java
@@ -1,11 +1,15 @@
 package kh.springboot.member.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import kh.springboot.board.model.entity.Board;
+import kh.springboot.member.model.entity.Member;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Board, Integer>{
 
+	Optional<Member> findByUsernameAndPwd(String username, String pwd)
 }
